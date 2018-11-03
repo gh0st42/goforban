78d9a925c9ce4fe407a4b2a72ae9bc55c41f9d7e
gh0st42 <gh0st@binarylab.eu>
2018-11-03 12:05:35 +0100

    fixed uuid api call

--- forban/common.go
+++ forban/common.go
@@ -25,7 +25,7 @@ type ForbanNodeEntry struct {
 
 // InitForban Initialize all relevant variables and update file index
 func InitForban() {
-	u1 := uuid.NewV4()
+	u1 := uuid.Must(uuid.NewV4())
 	MyUuid = u1.String()
 	thishost, _ := os.Hostname()
 	MyName = "goforban@" + thishost
